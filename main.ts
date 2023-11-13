a59b834eca139a9aabde528cde39409b016c6ec2
Savinu Nugith Dulwan Wimalasuriya <150595620+stu0320330@users.noreply.github.com>
2023-11-13 17:25:11 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+turtle.setPosition(1, 1)
+turtle.pen(TurtlePenMode.Down)
+for (let index = 0; index < 16; index++) {
+    turtle.turnRight()
+    turtle.forward(2)
+}
+basic.showString("Wlcome To Microbit")
